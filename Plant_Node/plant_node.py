8b1fee4e76798d1982763b79e7abe3a5baa0809d
JuliusSDev <julius.spiegelhalter@googlemail.com>
2024-05-17 10:04:41 +0200

    Added basic main-functions

---
+++ Plant_Node/plant_node.py
@@ -0,0 +1,17 @@
+# ------------------------ INCLUDES ------------------------
+import os
+import sys
+import logging
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+from inc.custom_logger import get_custom_logger
+
+# ------------------------ GLOBAL VARIABLES ------------------------
+# Get the custom logger
+logger = get_custom_logger('my_logger', level=logging.DEBUG)
+
+# ------------------------ MAIN FUNCTION ------------------------
+if __name__ == "__main__":
+    logger.debug("Starting in plant_node.py")
+    
