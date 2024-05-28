cd62d25afe28d127ed5d0ad893b519661d1e667e
JuliusSDev <julius.spiegelhalter@googlemail.com>
2024-05-28 13:07:06 +0200

    added messages

---
+++ inc/messages.py
@@ -0,0 +1,21 @@
+# We need different kind of messages:
+# Set Mode (CN -> PN) 
+# Set Mode config(CN -> PN) which Mode (Winter/Spring/Summer/Autumn) which humidity/soil moisture wanted
+# 
+# 
+# 
+# 
+# 
+# Send Status (PN -> CN) (ERRORCODE) and average humidity etc. every 12 hours
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
