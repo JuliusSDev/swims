12d5780999afde958729359bd82c498c884dbb02
JuliusSDev <julius.spiegelhalter@googlemail.com>
2024-05-28 13:50:02 +0200

    first connection established

--- Central_Node/central_node.py
+++ Central_Node/central_node.py
@@ -7,6 +7,7 @@ import socket
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 from inc.custom_logger import get_custom_logger
+import inc.messages
 
 # ------------------------ GLOBAL VARIABLES ------------------------
 # Get the custom logger
@@ -28,11 +29,12 @@ def setup_server():
 
 def main (server,):
     logger.debug(f"Entering function main()")
-    
+
     stop = False
     while not stop:
-        i = 0
-
+        client_socket, client_address = server.accept()
+        logger.info(f"Connection accepted under {client_address}")
+        client_socket.close()
 
 
 # ------------------------ MAIN FUNCTION ------------------------
--- Plant_Node/plant_node.py
+++ Plant_Node/plant_node.py
@@ -2,16 +2,36 @@
 import os
 import sys
 import logging
+import socket
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 from inc.custom_logger import get_custom_logger
+import inc.messages
 
 # ------------------------ GLOBAL VARIABLES ------------------------
 # Get the custom logger
 logger = get_custom_logger('my_logger', level=logging.DEBUG)
 
+# ------------------------ USER FUNCTION ------------------------
+def main (client):
+    logger.debug(f"Entering function main()")
+    
+    server_ip = "127.0.0.1"  # replace with the server's IP address
+    server_port = 8000 
+    client.connect((server_ip, server_port))
+    stop = False
+    #while not stop:
+
+         
+
 # ------------------------ MAIN FUNCTION ------------------------
 if __name__ == "__main__":
     logger.debug("Starting in plant_node.py")
-    
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        main(client)
+    except:
+        logger.warning('Keyboard interrupt detected')
+    client.close()
+    logger.info(f"Client socket closed.")
